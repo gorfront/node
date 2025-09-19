30d0d3590065c45006d6e29d9e74922206d84027
akselpapoyan1 <akselpapoyan123@gmail.com>
2025-09-19 18:51:39 +0400

    Add some constructor for forms

--- webapp/src/pages/NewIdeaPage/index.tsx
+++ webapp/src/pages/NewIdeaPage/index.tsx
@@ -1,5 +1,36 @@
 import { Segment } from "../../components/Segment";
+import { Textarea } from "../../components/Textarea";
+import { Input } from "../../components/Input";
+import { useFormik } from "formik";
 
 export const NewIdeaPage = () => {
-  return <Segment title="New Idea">Form will be here...</Segment>;
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      nick: "",
+      description: "",
+      text: "",
+    },
+    onSubmit: (values) => {
+      console.log("Form data", values);
+    },
+  });
+
+  return (
+    <Segment title="New Idea">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          formik.handleSubmit();
+        }}
+      >
+        <Input name="name" label="Name" formik={formik} />
+        <Input name="nick" label="Nick" formik={formik} />
+        <Input name="description" label="Description" formik={formik} />
+        <Textarea name="text" label="Text" formik={formik} />
+
+        <button type="submit">Create Idea</button>
+      </form>
+    </Segment>
+  );
 };
